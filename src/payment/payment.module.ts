2fcdb9686cdac44795fea19e32c59dc1ad5fd39c
ordila <ordila99@gmail.com>
2024-11-27 12:57:47 +0200

    fix payment controller

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
-import { PaymentController } from './payment.controller';
+import { PaymentsController } from './payment.controller';
 
 @Module({
-  controllers: [PaymentController],
+  controllers: [PaymentsController],
   providers: [PaymentService],
 })
 export class PaymentModule {}
